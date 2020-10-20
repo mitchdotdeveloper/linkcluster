b7bf4b8bf83458c74b4d2de5b92c70b40f0964d7
Mitch O'Hair <mitchell.ohair@gmail.com>
2020-10-20 10:08:27 -0700

    Develop (#4)
    * updated workflow
    
    * updated workflow
    
    * updated workflow
    
    * set up docker-compose for local postgres development and added istanbul for test coverage
    
    * updated .gitignore
    
    * Feature/setup api structure (#2)
    
    * added pg and accompanying types
    
    * Wired together barebones SOLID IOC BE
    
    * Added tests for UserRepository
    
    * Added tests for UserModel
    
    * Added and fixed all tests
    
    * Refactor/workflow (#3)
    
    * updated workflow file
    
    * updated workflow file to run also when the workflow directory has changes

---
+++ server/test/repositories/UserRepository.test.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-expressions */
+import 'reflect-metadata';
+import { expect } from 'chai';
+import db from 'connect';
+import container from 'inversify.config';
+import TYPES from 'inversifyTypes';
+import { after, before, describe, test } from 'mocha';
+import { UserDTO } from 'models/UserModel';
+import { QueryResult } from 'pg';
+import { UserRepository } from 'repositories/UserRepository';
+import { stub } from 'sinon';
+
+describe('UserRepository Suite', () => {
+  const userRepository: UserRepository = container.get<UserRepository>(
+    TYPES.UserRepository
+  );
+
+  const queryStub = stub(db, 'query');
+
+  before(() => {
+    // successful query
+    queryStub
+      .withArgs('SELECT username FROM users WHERE username = $1;', [
+        'usernameExists',
+      ])
+      .resolves(<QueryResult<UserDTO>>{
+        rows: [{ username: 'usernameExists' }],
+        rowCount: 1,
+      });
+
+    // unsuccessful query
+    queryStub
+      .withArgs('SELECT username FROM users WHERE username = $1;', [
+        'usernameDoesNotExist',
+      ])
+      .resolves(<QueryResult<never>>{
+        rows: [],
+        rowCount: 0,
+      });
+  });
+  after(() => {
+    queryStub.restore();
+  });
+
+  test('read() : finds user by the given username', async () => {
+    expect(await userRepository.read('usernameExists')).to.be.deep.equal(<
+      UserDTO
+    >{ username: 'usernameExists' });
+  });
+
+  test('read() : does not find user by the given username', async () => {
+    expect(await userRepository.read('usernameDoesNotExist')).to.be.null;
+  });
+});
