9270ea44ce4f01a75b6e7e288e5395cc0f436f86
Mitchell O'Hair <mitchell.ohair@gmail.com>
2021-01-07 15:30:11 -0800

    Fixed absolute import breaking test

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['node_modules/', '.next/'],
+  moduleDirectories: ['node_modules', 'src'],
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
