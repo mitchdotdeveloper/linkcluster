a8d63d1e58a44ca1da60ea131be0715023ec4447
Mitchell O'Hair <mitchell.ohair@gmail.com>
2021-01-07 15:41:43 -0800

    Removed redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,6 @@ import cors from 'cors';
 import { RegistrableController } from 'controllers/RegistrableController';
 import container from './inversify.config';
 import TYPES from './inversifyTypes';
-import { store } from './connectCache';
-import session from 'express-session';
 
 const app = express();
 
@@ -14,19 +12,6 @@ app.use(helmet());
 app.use(express.json());
 
 app.use(cors({ origin: 'http://localhost:5000' }));
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET!,
-    name: 'sessionId',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',
-    },
-    store: store(),
-  })
-);
 
 const controllers: RegistrableController[] = container.getAll<
   RegistrableController
